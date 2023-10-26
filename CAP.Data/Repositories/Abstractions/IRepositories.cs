6983c840cb7681e6a8329a3e3f9af092dcb0fa6a
Enes Esen <g.enesesen@gmail.com>
2023-10-26 23:58:26 +0300

    27..10.2023

--- CAP.Data/Repositories/Abstractions/IRepositories.cs
+++ CAP.Data/Repositories/Abstractions/IRepositories.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,17 @@ namespace CAP.Data.Repositories.Abstractions
     public interface IRepositories<T> where T : class, IEntityBase, new()
     {
         Task AddAsync(T entity);
+
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+
+        //Tek bir veri dönmesi için aşağıdaki işlem yapılır.
+        Task<T> GetAsync(Expression<Func<T, bool>> predicate = null, params Expression<Func<T, object>>[] includeProperties);
+
+        // 
+        Task<T> GetByGuidAsync(Guid id);
+        Task<T> UpdateAsync(T entity);
+        Task<T> DeleteAsync(T entity);
+        Task<T> HardDeleteAsync();
     }
 }
+// https://www.youtube.com/watch?v=Yn4TaQ3ws_M&list=PLrSCwxkucNmxFrrAsGm14Z-5Cu52MKrNr&index=14&t=639s
--- CAP.Data/Repositories/Concretes/Repositories.cs
+++ CAP.Data/Repositories/Concretes/Repositories.cs
@@ -10,10 +10,13 @@ using System.Threading.Tasks;
 
 namespace CAP.Data.Repositories.Concretes
 {
+    // T'nin bir class olduğunun belirtilmesi gerekiyor.
     public class Repositories<T> : IRepositories<T> where T : class, IEntityBase, new()
     {
+        // asing field
         private readonly AppDbContext _dbContext;
 
+        // constructer
         public Repositories(AppDbContext dbContext) 
         {
             this._dbContext = dbContext;
@@ -23,15 +26,41 @@ namespace CAP.Data.Repositories.Concretes
         private DbSet<T> Table { get => _dbContext.Set<T>(); }
         // Table'ı _dbContext ile set ediyoruz.
         // Böylece her yerde _dbContext'i kullanmayız.
+        // T nesnesi oluşturulan entity'leri kapsıyor.
+
+        // Expression metod
+        // params, tekrar expression oluşturmak için kullanılır.
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, 
+            params Expression<Func<T, object>>[] includeProperties)
+        {
+            IQueryable<T> query = Table;
+            if (predicate != null)
+            {
+                query = query.Where(predicate);
+            }
+            if (includeProperties.Any())
+            {
+                foreach (var item in includeProperties)
+                {
+                    query = query.Include(item);
+                }
+            }
+
+            return await query.ToListAsync();
+            // Interface'i fonksiyonun ismini alırız.
+        }
 
         public async Task AddAsync(T entity)
         {
+            // Task = Void, Void'in asekron olarak kodlamada kullanılan isimdir.
             await Table.AddAsync(entity);
             //AWAIT: Çağırdıktan sonra beklemesi için kullanılır.
+
+            // Bu metodun Interface'ini yazmamız gerekiyor bunun için IRepository'de metod oluşturulur.
         }
 
-        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>> predicate = null, 
-            params Expression<Func<T, object>>);
+
+
 
     }
 }
